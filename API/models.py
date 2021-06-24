a67222efe288af97cc159d69152a77bbf06beb95
Renato <lacerdarenato00@gmail.com>
2021-06-24 20:05:13 -0300

    Create serializer.py and rest_framework route

--- API/models.py
+++ API/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Veiculo (models.Model):
+
+    modelo = models.CharField(max_length=30)
+    marca = models.CharField(max_length=30)
+    ano = models.IntegerField(max_length=4)
+    cor = models.CharField(max_length=30)
+    placa = models.CharField(max_length=7)
+    chassi = models.CharField(max_length=17, unique=True)
+    dataCompra = models.DateField(auto_now_add=True)
+    valorCompra = models.DecimalField(decimal_places=2)
+
+    def __str__(self):
+        return self.modelo
