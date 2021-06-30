3c4631f09de281f3c7a4438edbe85c788c4bd25c
Renato <lacerdarenato00@gmail.com>
2021-06-30 00:38:09 -0300

    Create routing in Angular.

--- AgenciaDeCarros/urls.py
+++ AgenciaDeCarros/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('veiculos/', include('Veiculos.urls')),
+    path('vendas/', include('Vendas.urls')),
 ]
--- Ui/src/app/app-routing.module.ts
+++ Ui/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { VeiculosComponent } from './veiculos/veiculos.component';
+import { VendaComponent } from './venda/venda.component';
+
+const routes: Routes = [
+  {path:'veiculos', component:VeiculosComponent},
+  {path:'vendas', component:VendaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
